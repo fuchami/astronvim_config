f7c05926c8de2e76626d4856a02b661717aefd21
fuchami <fuchami23@icloud.com>
2023-08-30 15:14:13 +0900

    update

---
+++ plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        -- show hidden files.
+        visible = true,
+      },
+    },
+  },
+}
