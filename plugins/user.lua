6d3011e8fae6e651e69af095e08cff6a7d7f975b
fuchami <fuchami23@icloud.com>
2023-09-05 20:42:34 +0900

    update

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,14 +1,6 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
   {
     "TimUntersberger/neogit",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
@@ -186,5 +178,16 @@ return {
 
       return opts
     end,
+  },
+  -- rainbow-indent like VScode
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = function(_, opts)
+      -- Other blankline configuration here
+      return require("indent-rainbowline").make_opts(opts)
+    end,
+    dependencies = {
+      "TheGLander/indent-rainbowline.nvim",
+    },
   },
 }
