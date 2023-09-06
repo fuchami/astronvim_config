5a22d54c9b11476942cdb5bf13eb0e207a8e71b8
fuchami <fuchami23@icloud.com>
2023-09-06 22:36:35 +0900

    delete comment out

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,22 +1,6 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- {
-  --   "TimUntersberger/neogit",
-  --   version = "*", -- Use for stability; omit to use `main` branch for the latest features
-  --   event = "VeryLazy",
-  --   dependencies = {
-  --     "sindrets/diffview.nvim",
-  --   },
-  --   config = function()
-  --     require("neogit").setup {
-  --       integrations = {
-  --         diffview = true,
-  --       },
-  --     }
-  --   end,
-  -- },
-  -- rainbow-indent like VScode
   {
     "lukas-reineke/indent-blankline.nvim",
     opts = function(_, opts)
