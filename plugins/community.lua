1c99b0cbcadc5f48e565ddc1b7646558ea5a4f93
fuchami <fuchami23@icloud.com>
2024-03-21 23:15:18 +0900

    add todo-comments-nvim

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,6 +3,7 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
