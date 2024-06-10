a9cfe8be5fb1bcfa81ae798ce110a312ff189848
fuchami <fuchami23@icloud.com>
2024-06-11 02:07:01 +0900

    add lsp-signature-nvim pack

--- lua/community.lua
+++ lua/community.lua
@@ -7,7 +7,7 @@ return {
   -- import/override with your plugins folder
   "AstroNvim/astrocommunity",
 
-  -- ui
+  -- ui recipes
   { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
   { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
@@ -16,6 +16,9 @@ return {
   -- editing-support
   { import = "astrocommunity.indent.indent-rainbowline" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+
+  -- lsp
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
 
   -- pack
   { import = "astrocommunity.pack.lua" },
