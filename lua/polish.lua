d3738a414ff15c0a231c75b8316cc1b0019d3da5
fuchami <fuchami23@icloud.com>
2024-06-11 01:49:49 +0900

    add filetypes for hql,tpl to sql

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,18 +1,19 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
+vim.opt.relativenumber = false
+
 -- Set up custom filetypes
 vim.filetype.add {
   extension = {
-    foo = "fooscript",
+    hql = "sql",
+    tpl = "sql",
   },
   filename = {
-    ["Foofile"] = "fooscript",
+    -- ["Foofile"] = "fooscript",
   },
   pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
+    -- ["~/%.config/foo/.*"] = "fooscript",
   },
 }
