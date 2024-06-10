1cd8dd2a61d057726083c0d30fe469f2bf80b437
fuchami <fuchami23@icloud.com>
2024-06-11 02:12:20 +0900

    fix

--- lua/community.lua
+++ lua/community.lua
@@ -11,7 +11,6 @@ return {
   { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
   { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
-  { import = "astrocommunity.recipes.vscode-icons" },
 
   -- editing-support
   { import = "astrocommunity.indent.indent-rainbowline" },
