afc867ba7e281a6a6d494219dffdd81d02020720
fuchami <fuchami23@icloud.com>
2025-04-07 22:25:30 +0900

    update community

--- lua/community.lua
+++ lua/community.lua
@@ -1,27 +1,26 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
 
 ---@type LazySpec
 return {
-  -- import/override with your plugins folder
   "AstroNvim/astrocommunity",
+
+  -- colorschema
+  -- { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
 
   -- recipes
   { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
   { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
 
-  -- editing-support
-  { import = "astrocommunity.indent.indent-rainbowline" },
-  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
-
   -- markdown
   { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
 
   -- git
   { import = "astrocommunity.git.diffview-nvim" },
-  { import = "astrocommunity.git.octo-nvim" },
 
   -- icon
   { import = "astrocommunity.icon.mini-icons" },
