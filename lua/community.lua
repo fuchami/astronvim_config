e94f2094c1e14eb2901f971a9305fb102cce1367
fuchami <fuchami23@icloud.com>
2025-07-07 21:20:34 +0900

    add octo-nvim

--- lua/community.lua
+++ lua/community.lua
@@ -29,6 +29,7 @@ return {
 
   -- git
   { import = "astrocommunity.git.diffview-nvim" },
+  { import = "astrocommunity.git.octo-nvim" },
 
   -- icon
   { import = "astrocommunity.icon.mini-icons" },
