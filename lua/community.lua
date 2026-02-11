dad5ea067ab4722627651905e35edfae2b132bf5
fuchami <fuchami23@icloud.com>
2026-02-11 17:17:00 +0900

    update

--- lua/community.lua
+++ lua/community.lua
@@ -8,7 +8,7 @@ return {
 
   -- colorschema
   -- { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
-  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  -- { import = "astrocommunity.colorscheme.kanagawa-nvim" },
 
   -- AI
   { import = "astrocommunity.completion.avante-nvim", enabled = false },
