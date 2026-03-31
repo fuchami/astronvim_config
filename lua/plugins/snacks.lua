713a8e95701b363042c70aa7d7d9fb2a878c8239
fuchami <fuchami23@icloud.com>
2026-03-31 22:22:49 +0900

    update snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -22,11 +22,18 @@ return {
     },
     ---@class snacks.gh.Config
     gh = {},
+    ---@class snacks.dashboard.Config
+    dashboard = {
+      enabled = true,
+    },
   },
   keys = {
     { "<leader>gi", function() require("snacks").picker.gh_issue() end, desc = "GitHub Issues (open)" },
-    { "<leader>gI", function() require("snacks").picker.gh_issue { state = "all" } end, desc = "GitHub Issues (all)" },
-    { "<leader>gp", function() require("snacks").picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
+    {
+      "<leader>gp",
+      function() require("snacks").picker.gh_pr() end,
+      desc = "GitHub Pull Requests (open)",
+    },
     {
       "<leader>gP",
       function() require("snacks").picker.gh_pr { state = "all" } end,
