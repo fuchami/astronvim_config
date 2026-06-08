7b6b36c76c5c590b0871933b8155431034e8da41
fuchami <fuchami23@icloud.com>
2026-06-08 23:36:17 +0900

    update

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -64,7 +64,7 @@ return {
   -- media
   {
     "3rd/image.nvim",
-    build = false,       -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
     opts = {
       backend = "sixel", -- or "ueberzug" or "kitty"
     },
@@ -75,7 +75,7 @@ return {
     dependencies = {
       { "3rd/image.nvim", opts = {} }, -- you'd probably want to configure image.nvim manually instead of doing this
     },
-    opts = {                           -- you can just pass {}, defaults below
+    opts = { -- you can just pass {}, defaults below
       events = {
         render_buffer = { "InsertLeave", "BufWinEnter", "TextChanged" },
         clear_buffer = { "BufLeave" },
@@ -83,11 +83,11 @@ return {
       renderer_options = {
         mermaid = {
           background = nil, -- nil | "transparent" | "white" | "#hex"
-          theme = nil,      -- nil | "default" | "dark" | "forest" | "neutral"
-          scale = 1,        -- nil | 1 (default) | 2  | 3 | ...
-          width = nil,      -- nil | 800 | 400 | ...
-          height = nil,     -- nil | 600 | 300 | ...
-          cli_args = nil,   -- nil | { "--no-sandbox" } | { "-p", "/path/to/puppeteer" } | ...
+          theme = nil, -- nil | "default" | "dark" | "forest" | "neutral"
+          scale = 1, -- nil | 1 (default) | 2  | 3 | ...
+          width = nil, -- nil | 800 | 400 | ...
+          height = nil, -- nil | 600 | 300 | ...
+          cli_args = nil, -- nil | { "--no-sandbox" } | { "-p", "/path/to/puppeteer" } | ...
         },
         plantuml = {
           charset = nil,
@@ -102,9 +102,9 @@ return {
           cli_args = nil, -- nil | { "--pad", "0" } | ...
         },
         gnuplot = {
-          size = nil,     -- nil | "800,600" | ...
-          font = nil,     -- nil | "Arial,12" | ...
-          theme = nil,    -- nil | "light" | "dark" | custom theme string
+          size = nil, -- nil | "800,600" | ...
+          font = nil, -- nil | "Arial,12" | ...
+          theme = nil, -- nil | "light" | "dark" | custom theme string
           cli_args = nil, -- nil | { "-p" } | { "-c", "config.plt" } | ...
         },
       },
