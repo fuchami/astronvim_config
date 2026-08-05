8cc07f116b34646d707057788c102c8490494b96
fuchami <fuchami23@icloud.com>
2026-08-06 00:10:11 +0900

    feat(ipynb): add jupynvim

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -12,6 +12,19 @@ return {
       },
     },
   },
+  {
+    "sheng-tse/jupynvim",
+    build = function(plugin)
+      local install = loadfile(plugin.dir .. "/lua/jupynvim/install.lua")()
+      install.run(plugin)
+    end,
+    config = function()
+      require("jupynvim").setup {
+        log_level = "info",
+        image_renderer = "kitty",
+      }
+    end,
+  },
   -- markdown
   {
     "MeanderingProgrammer/render-markdown.nvim",
